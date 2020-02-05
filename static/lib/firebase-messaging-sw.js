fd6d2d973ee5ccdf24b509e1289fd6941618f45a
Julian Lam <julian@nodebb.org>
2020-02-04 19:49:45 -0500

    fix: retrieving proper app config from db instead of hardcoded values

--- static/lib/firebase-messaging-sw.js
+++ static/lib/firebase-messaging-sw.js
@@ -5,15 +5,7 @@
 importScripts('https://www.gstatic.com/firebasejs/7.7.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.7.0/firebase-messaging.js');
 
-firebase.initializeApp({
-	apiKey: 'AIzaSyBi3-G3s3W-yvgOZisDkutWACzOnLVACdA',
-	authDomain: 'playground-3f15c.firebaseapp.com',
-	databaseURL: 'https://playground-3f15c.firebaseio.com',
-	projectId: 'playground-3f15c',
-	storageBucket: 'playground-3f15c.appspot.com',
-	messagingSenderId: '39485711083',
-	appId: '1:39485711083:web:ef4d842f4b0bc2aaba54ae',
-});
+firebase.initializeApp(/* config */);
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
